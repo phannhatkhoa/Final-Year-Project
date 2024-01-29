66781757906b2307b2a6c2527c87ba0567f3fcea
Khoa Nhat <khoapngcd201807@fpt.edu.vn>
2024-01-29 11:03:00 +0700

    Fix bug

--- Admin/Route/LoginRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const loginController = require('../Controllers/loginController');
-const { loginValidation } = require('../Middlewares/loginMiddleware');
-const loginRouter = express.Router();
-
-loginRouter.post('/login',loginValidation ,loginController);
-
-module.exports = loginRouter;
