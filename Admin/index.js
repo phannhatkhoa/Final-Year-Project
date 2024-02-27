78851db5fded2caff4af59f90d49e143112d3b81
Khoa Nhat <khoapngcd201807@fpt.edu.vn>
2024-02-27 09:39:55 +0700

    Create routes for Login, Register, Profile, Password, Profile

--- Admin/index.js
+++ Admin/index.js
@@ -1,31 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoDBConnector } = require('./database');
-const loginRouter = require('./Route/loginRoute');
+const loginRouter = require('./Route/LoginRoute');
 
 const app = express();
-const databaseClass = new MongoDBConnector(); // Tạo thực thể của MongoDBConnector
+const databaseClass = new MongoDBConnector();
 
 app.use(bodyParser.json());
-
-const myLogger = function (req, res, next) {
-    const { password, email } = req.body;
-
-    if (password === '123' && email === 'test@gmail.com') {
-        next();
-    } else {
-        res.status(400).json({
-            error: 'Invalid credentials',
-        });
-    }
-};
-
 // app.post('/login', myLogger, (req, res) => {
 //     // You should not log or send sensitive information like passwords in the response.
 //     res.json({ message: 'Login successful' });
 // });
 
-app.use('/abc',loginRouter);
+app.use('/auth',loginRouter);
+
 
 databaseClass.connect();
 const port = 3000;
--- Client/ecommerce-2nd/src/App.js
+++ Client/ecommerce-2nd/src/App.js
@@ -1,18 +1,62 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
+// import Login from './Pages/Login';
+// import Register from './Pages/Register';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
+/**all components */
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username></Username>
+  },
+  {
+    path: "/register",
+    element: <Register></Register>
+  },
+  {
+    path: "/Password",
+    element: <Password></Password>
+  },
+  {
+    path: "/profile",
+    element: <Profile></Profile>
+  },
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>
+  },
+  {
+    path: "/reset",
+    element: <Reset></Reset>
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <Register />
-        {/* <Login /> */}
-      </div>
-    </div>
+    // <div className="App">
+    //   <div className="container">
+    //     <Register />
+    //     {/* <Login /> */}
+    //   </div>
+    // </div>
+    <main>
+      <RouterProvider router={router}>
+      </RouterProvider>
+    </main>
   );
 }
 
